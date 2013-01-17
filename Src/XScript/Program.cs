062c6483016c78083e7a2ca067519ed06671a670
nnhy <nnhy@vip.qq.com>
2013-01-17 07:41:23 +0000

    新生命C#文件脚本引擎v1.0完成，从今以后，可以用C#代替vbs/js/bat/Python/Perl来编写脚本处理日常工作了！

--- Src/XScript/Program.cs
+++ Src/XScript/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NewLife.Log;
+using System.IO;
+using NewLife.Reflection;
 
 namespace XScript
 {
@@ -8,6 +11,39 @@ namespace XScript
     {
         static void Main(string[] args)
         {
+#if DEBUG
+            XTrace.UseConsole();
+#endif
+            XTrace.TempPath = "XTemp";
+
+            try
+            {
+                if (args != null && args.Length == 0)
+                {
+                    // TODO: 显示帮助菜单
+                    Console.WriteLine("显示帮助信息");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    var file = args[0];
+#if DEBUG
+                    Console.WriteLine("执行脚本：{0}", file);
+#endif
+
+                    var code = File.ReadAllText(file);
+                    var sc = ScriptEngine.Create(code, false);
+                    sc.Invoke();
+                }
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+
+#if DEBUG
+            Console.ReadKey();
+#endif
         }
     }
 }
--- Src/XScript/Properties/AssemblyInfo.cs
+++ Src/XScript/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
+[assembly: AssemblyProduct("XXScript")]
 [assembly: AssemblyTitle("新生命C#脚本引擎")]
 [assembly: AssemblyDescription("用于编译执行C#文件脚本")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("新生命开发团队")]
-[assembly: AssemblyProduct("NewLife.Core")]
 [assembly: AssemblyCopyright("\x00a92002-2013 新生命开发团队")]
 [assembly: AssemblyTrademark("四叶草")]
 [assembly: AssemblyCulture("")]
