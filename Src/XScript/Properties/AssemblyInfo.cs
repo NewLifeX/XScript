e18b45daaca44c7d2f0a2795a95109f5e334579e
nnhy <nnhy@vip.qq.com>
2013-01-17 07:09:56 +0000

    v1.0.2013.0117   创建XScript

--- Src/XScript/Properties/AssemblyInfo.cs
+++ Src/XScript/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("XScript")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("新生命C#脚本引擎")]
+[assembly: AssemblyDescription("用于编译执行C#文件脚本")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("XScript")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("新生命开发团队")]
+[assembly: AssemblyProduct("NewLife.Core")]
+[assembly: AssemblyCopyright("\x00a92002-2013 新生命开发团队")]
+[assembly: AssemblyTrademark("四叶草")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
@@ -32,5 +32,10 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.2013.0117")]
+
+/*
+ * v1.0.2013.0117   创建XScript
+ *
+*/
