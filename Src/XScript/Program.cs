17e75e573c5f068508cfddf32a527b604e559542
nnhy <nnhy@vip.qq.com>
2013-01-17 12:46:07 +0000

    增加到发送到菜单

--- Src/XScript/Program.cs
+++ Src/XScript/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using NewLife.Log;
 using NewLife.Reflection;
+using System.Reflection;
 
 namespace NewLife.XScript
 {
@@ -27,14 +28,33 @@ namespace NewLife.XScript
             if (Config.Debug) XTrace.UseConsole();
 
             XTrace.TempPath = "XTemp";
+            Console.WriteLine("©");
 
             #region 版权信息
             if (!Config.NoLogo)
             {
+                var asmx = AssemblyX.Create(Assembly.GetExecutingAssembly());
+
                 var oldcolor = Console.ForegroundColor;
-                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("欢迎使用{0}！", asmx.Title);
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("本工具由新生命开发团队开发，{0}！", asmx.Description);
+                Console.WriteLine("版权所有：{0}", asmx.Asm.GetCustomAttributeValue<AssemblyCopyrightAttribute, String>());
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("XGZip采用GZip压缩算法，所压缩的单文件使用WinRar等工具可以直接解压，所压缩的多文件因为采用特别的格式，必须由XGZip或者调用核心库NewLife.Core.dll的IOHelper来解压，WinRar解压只会得到一个文件包。");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("温馨提醒：{0}会自己添加到资源管理器右键菜单中的\"发送到\"中，便于使用！", asmx.Name);
+                Console.WriteLine();
+
+                Console.ForegroundColor = oldcolor;
             }
             #endregion
+
+            SetSendTo();
 
             try
             {
@@ -64,6 +84,30 @@ namespace NewLife.XScript
 #if DEBUG
             Console.ReadKey();
 #endif
+        }
+
+        static void SetSendTo()
+        {
+            var dir = Environment.GetFolderPath(Environment.SpecialFolder.SendTo);
+            if (!Directory.Exists(dir)) return;
+
+            var asmx = AssemblyX.Create(Assembly.GetExecutingAssembly());
+
+            var file = Path.Combine(dir, asmx.Title + ".lnk");
+
+            try
+            {
+                var sc = new Shortcut();
+                sc.Path = Assembly.GetEntryAssembly().Location;
+                //sc.Arguments = "启动参数";
+                sc.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                sc.Description = asmx.Description;
+                sc.Save(file);
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteLine(ex.ToString());
+            }
         }
     }
 }
--- Src/XScript/Properties/AssemblyInfo.cs
+++ Src/XScript/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyProduct("XXScript")]
+[assembly: AssemblyProduct("XScript")]
 [assembly: AssemblyTitle("新生命C#脚本引擎")]
 [assembly: AssemblyDescription("用于编译执行C#文件脚本")]
 [assembly: AssemblyConfiguration("")]
