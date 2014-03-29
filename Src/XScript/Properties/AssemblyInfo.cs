516f3bee4a8d89fcba8bfd7172c0b5f7de6d80a8
Stone <nnhy@vip.qq.com>
2014-03-29 15:55:09 +0000

    v1.6.2014.0329   支持按c键让脚本再次执行，跳过编译过程

--- Src/XScript/Properties/AssemblyInfo.cs
+++ Src/XScript/Properties/AssemblyInfo.cs
@@ -29,10 +29,12 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.*")]
-[assembly: AssemblyFileVersion("1.5.2014.0109")]
+[assembly: AssemblyVersion("1.6.*")]
+[assembly: AssemblyFileVersion("1.6.2014.0329")]
 
 /*
+ * v1.6.2014.0329   支持按c键让脚本再次执行，跳过编译过程
+ * 
  * v1.5.2014.0109   控制端可以直接输入用户代码或者脚本路径
  * 
  * v1.4.2014.0107   从代码中读取配置
